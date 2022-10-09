adb7282a76975e12ffb3753d77c9405f17647af5
unknown <amv.harsh@gmail.com>
2022-10-09 19:48:15 +0530

    made the changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
 import "./global.css";
 import SignUp from "./pages/SignUp/SignUp";
+import Waiting from "./pages/Waiting/Waiting";
 
 function App() {
-  return <SignUp />;
+    return(<Waiting/>);
 }
 
 export default App;
---
+++ src/pages/Waiting/Waiting.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Lounge from '../../components/Lounge';
+
+function Waiting(){
+    return(
+        <Lounge/>
+    );
+}
+export default Waiting;
