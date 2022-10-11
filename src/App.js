3f5da54c3ecfe2430dad0fb00f27348ebfbf0786
abhinavvodedeveloper <abhinavssr2003@gmail.com>
2022-10-11 13:56:17 +0530

    Commented out the lines

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import "./App.css";
-// import "./global.css";
+import "./global.css";
 import Home from "./pages/Home/Home"
-// import SignUp from "./pages/SignUp/SignUp";
-// import Waiting from "./pages/Waiting/Waiting";
+import SignUp from "./pages/SignUp/SignUp";
+import Waiting from "./pages/Waiting/Waiting";
 
 function App() {
     return <Home/>;
