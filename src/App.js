2fb9a49ed66fc20bb1622d93c06ff19b89017cfa
Advait Jain <89680252+Zoronium@users.noreply.github.com>
2022-10-11 17:01:42 +0530

    Fix: merge Error

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import Home from "./pages/Home/Home"
 import SignUp from "./pages/SignUp/SignUp";
 import Waiting from "./pages/Waiting/Waiting";
 import Result from "./pages/Result/Result";
+import QuizClient from "./pages/QuizOngoing/QuizClient";
 
 function App() {
-    return <Home/>;
+    return (<QuizClient />);
 }
 
 export default App;
